b9b5c343bcaf8bad77d7b28c64e32e6643892f85
josterand <josterand@outlook.com>
2024-11-06 22:43:43 +0700

    feat: centered floating dual-bar Polybar & ALSA module

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
+# MIT License
+# 
+# Copyright (c) 2024 Jonathan Steve Roland (josterand)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # Terminate already running bar instances
 killall -q polybar
@@ -7,4 +28,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 2; done
 
 # Launch the bar
-polybar -q main-display -c ~/.config/polybar/config.ini &
+polybar -q top-bar -c ~/.config/polybar/config.ini &
+polybar -q bottom-bar -c ~/.config/polybar/config.ini &
